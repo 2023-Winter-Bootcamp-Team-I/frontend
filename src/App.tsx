1f32c208b05a3cfd98507e18d2fc4c76a1f980b5
youlimsongs <giuhki@gmail.com>
2024-01-04 05:16:11 +0900

    add project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import './App.css'
+import { RouterProvider } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import router from './utils/Routes';
+
 
 function App() {
 
   return (
     <>
+      <RecoilRoot>
+          <RouterProvider router={router} />
+      </RecoilRoot>
     </>
   )
 }
