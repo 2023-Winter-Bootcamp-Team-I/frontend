5e2b32f132cbdb468f446e56486eb1adf71f50ba
yyg47 <yyg470603@gmail.com>
2024-01-09 03:19:46 +1100

    adjust css

--- src/components/LogInModal.tsx
+++ src/components/LogInModal.tsx
@@ -9,21 +9,21 @@ function LogInModal() {
       <div className="fixed inset-0 z-10 w-screen">
         <div className="flex min-h-screen justify-center p-4 text-center sm:items-center sm:p-0">
           {/* Modal code starts from the below line */}
-          <div className="relative rounded-lg shadow-xl transition-all my-8 w-2/3">
+          <div className="relative rounded-lg shadow-xl transition-all my-8 w-1/2">
             <div className="flex flex-row h-[60vh]">
               <div className="flex flex-col bg-loginBlue basis-1/3">
                 <div className="flex basis-2/3 justify-center">
-                  <img className="mx-auto min-h-full w-1/2" src={heartImg} alt="heart_character" />
+                  <img className="mx-auto min-h-full w-2/3" src={heartImg} alt="heart_character" />
                 </div>
                 <div className="flex basis-1/3 font-dongle text-white text-9xl justify-center align-bottom">
                   북그북그
                 </div>
               </div>
               <div className="flex flex-col bg-mainColor basis-2/3 h-full">
-                <div className="flex text-white font-jua text-5xl justify-center mt-20 mb-[8vh]">로그인</div>
+                <div className="flex text-white font-jua text-5xl justify-center mt-20 mb-[7vh]">로그인</div>
                 <div className="flex justify-center">
                   <img
-                    className="flex mx-auto min-h-full w-auto opacity-50 -mr-20 ml-10 w-10"
+                    className="flex mx-auto min-h-full w-12 opacity-50 -mr-20 ml-10 w-10"
                     src={letterImg}
                     alt="letter_image"
                   />
@@ -33,12 +33,12 @@ function LogInModal() {
                     type="email"
                     placeholder="이메일을 입력해주세요"
                     required
-                    className="block w-3/4 text-xl rounded-full border-0 px-20 py-3 border-2 border-shadowGray"
+                    className="block w-3/4 text-xl rounded-full border-0 px-24 py-[18px] border-2 border-shadowGray"
                   />
                 </div>
-                <div className="flex mt-8 mb-[8vh] justify-center">
+                <div className="flex mt-8 mb-[7vh] justify-center">
                   <img
-                    className="flex mx-auto min-h-full w-auto opacity-50 -mr-20 ml-10 w-10"
+                    className="flex mx-auto min-h-full w-12 opacity-50 -mr-20 ml-10 w-10"
                     src={lockImg}
                     alt="lock_image"
                   />
@@ -48,15 +48,15 @@ function LogInModal() {
                     type="password"
                     placeholder="비밀번호를 입력해주세요"
                     required
-                    className="block w-3/4 text-xl rounded-full border-0 px-20 py-3 border-2 border-shadowGray"
+                    className="block w-3/4 text-xl rounded-full border-0 px-24 py-[18px] border-2 border-shadowGray"
                   />
                 </div>
                 <div className="flex justify-center">
                   <div className="flex flex-row w-3/4">
-                    <button className="flex basis-1/2 justify-center font-jua rounded-3xl bg-white py-4 text-2xl leading-6 text-loginBlue mr-2">
+                    <button className="flex basis-1/2 justify-center font-jua rounded-3xl bg-white py-4 text-3xl leading-7 text-loginBlue mr-2">
                       로그인
                     </button>
-                    <button className="flex basis-1/2 justify-center font-jua rounded-3xl bg-loginBlue py-4 text-2xl leading-6 text-white ml-2">
+                    <button className="flex basis-1/2 justify-center font-jua rounded-3xl bg-loginBlue py-4 text-3xl leading-7 text-white ml-2">
                       회원 가입
                     </button>
                   </div>
