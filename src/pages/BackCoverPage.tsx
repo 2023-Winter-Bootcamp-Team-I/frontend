d56ee87130e3e42787edd3550c0d2b6f1d2dc755
yyg47 <yyg470603@gmail.com>
2024-01-11 14:33:41 +1100

    update

--- src/pages/BackCoverPage.tsx
+++ src/pages/BackCoverPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import barcodeImg from '@/assets/images/barcode.svg';
 import prevButtonImg from '@/assets/images/prevButton.svg';
-import nextButtonImg from '@/assets/images/nextButton.svg';
 
 function BackCoverPage() {
   return (
@@ -10,13 +9,10 @@ function BackCoverPage() {
         <div className="flex basis-1/2 h-screen justify-center content-center">
           <div className="flex bg-bookCoverBack h-[90%] w-[97%] mt-8 ml-24 rounded-3xl">
             <div className="flex flex-col bg-bookCoverFront h-full w-full mt-6 z-10 ml-6 -mr-6 rounded-3xl justify-center items-end ">
-              <div className="relative top-[55%] bg-bookCoverLine h-1/6 w-full"></div>
-              <img className="z-20 w-80 mr-10 mt-[85%]" src={barcodeImg} alt="robot_character" />
+              <div className="relative bg-bookCoverLine h-1/5 w-full mt-[500px]"></div>
+              <img className="z-20 w-80 mr-10 mt-16" src={barcodeImg} alt="robot_character" />
             </div>
           </div>
-        </div>
-        <div className="absolute top-[45%] right-10 bg-moveButtonColor rounded-full h-24 w-24 z-20 items-end">
-          <img className="w-20 z-20 mx-auto my-0" src={nextButtonImg} alt="next_button" />
         </div>
         <div className="absolute top-[45%] bg-moveButtonColor rounded-full h-24 w-24 ml-10 z-20">
           <img className="flex w-20 z-20 mx-auto -ml-[0.01px] mt-[0.02px] my-0" src={prevButtonImg} alt="prev_button" />
