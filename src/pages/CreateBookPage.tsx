2434187031e000834a283014add4f5c803028e45
youlimsongs <giuhki@gmail.com>
2024-01-12 16:12:50 +0900

    chore: 책 생성 페이지 수정완료

--- src/pages/CreateBookPage.tsx
+++ src/pages/CreateBookPage.tsx
@@ -2,28 +2,25 @@ import QuestionMark from '@/assets/image/CreateBook/QuestionMark.svg';
 const CreateBookPage = () => {
   return (
     // 배경 색 흐릿하게
-    <div className="w-screen h-screen bg-mainColor bg-opacity-15 relative z-5">
-      <div className="flex flex-col justify-center h-screen">
+    <div className="w-screen h-screen bg-white bg-opacity-25 relative z-5">
+      <div className="flex mx-auto my-0 justify-center h-screen w-[1500px]">
         {/* 가장 큰 박스 */}
-        <div className=" h-full mx-24 my-3 bg-[#93C4FF] rounded-2xl z-10">
-          <div className="flex relative">
-            <div className="flex items-center justify-center h-full">
-              {/* 중간 띠 */}
-              <div className="w-[5px] h-full  left-[50%] absolute bg-[#1A7FD5]" />
-            </div>
+        <div className=" w-[1450px] px-0 my-3 bg-[#93C4FF] rounded-2xl z-10">
+          <div className="flex flex-row w-full h-full">
             {/* 왼쪽 박스 */}
-            <div className=" w-1/2 flex justify-center items-center flex-col">
-              <div className="box-border  basis- 1/2 w-[580px] h-[640px] top-[2.5%] left-[3.5%] absolute bg-white opacity-45 rounded-2xl z-20">
-                <img src={QuestionMark} className="w-1/7 left-[20%]  top-[10%] z-50 absolute  "></img>
-                <div className="box-border w-[540px] h-[300px] top-[23.5%] left-[9%] relative bg-white rounded-2xl z-50" />
+            <div className=" w-1/2 flex justify-center items-center flex-col px-[2.5rem] py-8 border-[#7bb6ff] border-y-4 border-l-4 border-solid rounded-2xl">
+              <div className="flex flex-col items-center justify-around box-border px-2 w-full h-full bg-white bg-opacity-45  rounded-2xl z-20">
+                <img src={QuestionMark} className=" mt-12 h-1/4 z-30"></img>
+                <div className="box-border mt-4 h-[55%] w-[95%] bg-[#fdfdfd] bg-opacity-70 rounded-2xl z-30" />
               </div>
             </div>
-            <div className="box-border w-1/6 "></div>
+            {/* 중간 띠 */}
+            <div className="w-[4px] mt-4 h-[96.5%] z-30 rounded-l bg-[#7bb6ff]" />
             {/* 오른쪽 박스 */}
-            <div className=" w-1/2  flex justify-center items-center flex-col">
-              <div className=" box-border w-[580px] h-[640px] top-[2.5%] left-[53.5%] absolute bg-white opacity-45 rounded-2xl z-20">
-                <img src={QuestionMark} className="w-1/7 left-[70%]  top-[10%] z-50 absolute"></img>
-                <div className="box-border w-[540px] h-[300px] top-[24.5%] left-[-8%] relative bg-white rounded-2xl z-50"></div>
+            <div className=" w-1/2 flex justify-center items-center flex-col px-[2.5rem] py-8 border-[#7bb6ff] border-y-4 border-r-4 border-solid rounded-2xl">
+              <div className="flex flex-col items-center justify-around box-border px-2 w-full h-full bg-white bg-opacity-45  rounded-2xl z-20">
+                <img src={QuestionMark} className=" mt-12 h-1/4 z-30"></img>
+                <div className="box-border mt-4 h-[55%] w-[95%] bg-[#fdfdfd] bg-opacity-70 rounded-2xl z-30" />
               </div>
             </div>
           </div>
