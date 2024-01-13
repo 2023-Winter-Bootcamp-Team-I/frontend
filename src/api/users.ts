a218c1e5de7e1b1cd56a43efc2837c8431058c08
SeHoon <wer5136@naver.com>
2024-01-14 02:58:35 +0900

    feat:회원가입 api연동 성공 (signup페이지에 로그인 모달 임시적으로 삭제했습니다

---
+++ src/api/users.ts
@@ -0,0 +1,23 @@
+// import api from './api';
+
+import axios from 'axios';
+
+interface SignUpData {
+  password: string;
+  email: string;
+  name: string;
+}
+
+export const signUpUser = async (userData: SignUpData): Promise<void> => {
+  try {
+    const response = await axios.post('/api/v1/users/signup/', userData);
+
+    // 서버 응답이 성공인 경우
+    console.log('User created:', response.data);
+    // 추가적인 성공 처리 (예: 리다이렉트)
+  } catch (error) {
+    // 서버 응답이 실패인 경우
+    // console.error('Error creating user:', error.message);
+    // 추가적인 실패 처리 (예: 에러 메시지 표시)
+  }
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import * as path from "path";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgrPlugin from "vite-plugin-svgr";
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,12 +10,12 @@ export default defineConfig({
     port: 3000,
     host: true,
     proxy: {
-      // "/": "http://localhost:8080",
+      '/api': 'http://localhost:8000',
     },
   },
   resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
