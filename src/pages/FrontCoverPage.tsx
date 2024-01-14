35ba3ef58895fe11fec71e806ac663c39577e8f7
youlimsongs <giuhki@gmail.com>
2024-01-15 04:00:03 +0900

    feat: webSocket test

--- src/pages/FrontCoverPage.tsx
+++ src/pages/FrontCoverPage.tsx
@@ -1,9 +1,75 @@
-import React from 'react';
 import robotImg from '@/assets/images/robot.svg';
 import prevButtonImg from '@/assets/images/prevButton.svg';
 import nextButtonImg from '@/assets/images/nextButton.svg';
+import { useEffect, useState } from 'react';
+
+type WsData = {
+  type: string;
+  pageCnt: number;
+  userName?: string;
+  fairyTale?: string;
+  gender?: string;
+  age?: number;
+  language?: string;
+  choice?: number;
+  koContent?: string;
+  enContent?: string;
+};
 
 function FrontCoverPage() {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const [message, setMessage] = useState<string>('');
+
+  const websocket = (data: WsData): void => {
+    const ws = new WebSocket('ws://localhost:8000/');
+
+    ws.onopen = () => {
+      ws.send(JSON.stringify(data));
+
+      setSocket(ws);
+    };
+
+    ws.onmessage = (event) => {
+      // Buffer를 문자열로 변환
+      const data = JSON.parse(event.data.toString());
+      console.log('Message from server:', data.message);
+      const message: string = data.message;
+
+      //한글자씩 받아오는?
+      setMessage((m) => m + message);
+    };
+
+    ws.onclose = () => {
+      setSocket(null);
+    };
+  };
+
+  const tempdata: WsData = {
+    type: 'ing',
+    pageCnt: 1,
+    choice: 1,
+    koContent: '나중에 state값 들어갈듯',
+    enContent: 'later state값 들어갈듯',
+  };
+
+  useEffect(() => {
+    websocket(tempdata);
+  });
+
+  //스트림 데이터 받아오는거 출력
+  console.log(message);
+
+  //나중에 선택한 스토리 보낼 때
+  socket?.send(
+    JSON.stringify({
+      type: 'ing',
+      pageCnt: 1,
+      choice: '1',
+      koContent: '나중에 state값 들어갈듯',
+      enContent: 'later state값 들어갈듯',
+    })
+  );
+
   return (
     <div className="w-screen h-screen bg-mainColor bg-opacity-15 relative z-10">
       <div className="absolute top-[45%] left-[2%] bg-moveButtonColor rounded-full h-24 w-24 ml-10 z-20">
