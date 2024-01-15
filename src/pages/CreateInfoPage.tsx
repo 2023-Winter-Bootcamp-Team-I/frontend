e35cd93e45fc5f93b2260fd2e1c014aa67683ce6
SeHoon <wer5136@naver.com>
2024-01-16 01:28:39 +0900

    bugfix 유저 정보 페이지 css 수정 완료!

--- src/pages/CreateInfoPage.tsx
+++ src/pages/CreateInfoPage.tsx
@@ -5,84 +5,84 @@ import Pencil from '@/assets/image/CreateInfo/Pencil.svg';
 const CreateInfoPage = () => {
   return (
     // 왼쪽 오른쪽 전체 div
-    <div className="flex w-screen h-screen justify-center items-center">
+    <div className="flex w-[1300px] h-screen justify-center items-center ml-28">
       {/* 뒤로가는 버튼 */}
       <button>
-        <img src={BackArrow} className="px-12 top-[5%] absolute"></img>
+        <img src={BackArrow} className="left-[0%] top-[0%] absolute"></img>
       </button>
       {/* 왼쪽 전체 div */}
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center mr-10 gap-5">
         {/* 이름 div  */}
-        <div className="flex flex-col">
+        <div className="flex flex-col w-full mb-5 ">
           {/* 이름 1 div  */}
-          <div className=" h-10rem justify-center items-center flex flex-row bg-[#659AFF] rounded-full">
-            <img src={Pencil} className="h-10rem w-1/5 mb-10 "></img>
-            <p className="font-[Jua] text-3xl text-white">너의 이름은 뭐야?</p>
+          <div className=" w-[95%] h-12 items-center flex flex-row bg-[#659AFF] rounded-full">
+            <img src={Pencil} className="w-1/5 mb-10 "></img>
+            <p className="font-[Jua] text-3xl mt-1 text-white pr-10">너의 이름은 뭐야?</p>
           </div>
           {/* 이름 2 div */}
-          <div>
+          <div className="flex w-full">
             <input
               type="text"
-              placeholder="이름 쓰기"
-              className="mt-4  px-5 h-[23%] text-3xl font-['Jua'] rounded-full "
+              placeholder="이름 입력"
+              className="h-10 w-[95%] mt-4 px-5 text-2xl font-['Jua'] rounded-full "
             />
           </div>
         </div>
         {/* 성별 div  */}
-        <div className="flex flex-col">
+        <div className="flex flex-col w-full mb-5">
           {/* 성별 1 div  */}
-          <div className=" flex items-center  flex-row bg-[#659AFF] rounded-full">
+          <div className="w-[95%] h-12 flex items-center  flex-row bg-[#659AFF] rounded-full">
             <img src={Pencil} className="w-1/5 mb-10 "></img>
-            <p className="font-[Jua] text-3xl text-white">너의 성별은 뭐야?</p>
+            <p className="font-[Jua] text-3xl mt-1 text-white pr-10">너의 성별은 뭐야?</p>
           </div>
           {/* 성별 2 div */}
-          <div>
-            <input
-              type="text"
-              placeholder="이름 쓰기"
-              className="mt-4  px-5 h-[23%] text-3xl font-['Jua'] rounded-full "
-            />
+          <div className="flex flex-row w-[95%] gap-5">
+            <button className="w-1/2 h-10  bg-white text-[#898989] rounded-full text-2xl mt-3 font-jua">남자</button>
+            <button className="w-1/2 h-10 bg-white text-[#898989] rounded-full text-2xl mt-3 font-jua">여자</button>
           </div>
         </div>
         {/* 나이 div  */}
-        <div className="flex flex-col">
+        <div className="flex flex-col w-full mb-5 ">
           {/* 나이 1 div  */}
-          <div className=" flex items-center  flex-row bg-[#659AFF] rounded-full">
+          <div className="w-[95%] h-12 flex items-center  flex-row bg-[#659AFF] rounded-full">
             <img src={Pencil} className="w-1/5 mb-10 "></img>
-            <p className="font-[Jua] text-3xl text-white">너의 이름은 뭐야?</p>
+            <p className="font-[Jua] text-3xl mt-1 text-white pr-16">너는 몇 살이야?</p>
           </div>
           {/* 나이 2 div */}
-          <div>
+          <div className="">
             <input
               type="text"
-              placeholder="이름 쓰기"
-              className="mt-4  px-5 h-[23%] text-3xl font-['Jua'] rounded-full "
+              placeholder="나이 입력"
+              className=" w-[95%] h-10 mt-4  px-5 text-2xl font-['Jua'] rounded-full "
             />
           </div>
         </div>
         {/* 한글 영어 div  */}
-        <div className="flex flex-col">
+        <div className="flex flex-col w-full mb-5 ">
           {/* 한글 영어 1 div  */}
-          <div className=" flex items-center  flex-row bg-[#659AFF] rounded-full">
+          <div className="w-[95%] h-12  flex items-center  flex-row bg-[#659AFF] rounded-full">
             <img src={Pencil} className="w-1/5 mb-10 "></img>
-            <p className="font-[Jua] text-3xl text-white">너의 이름은 뭐야?</p>
+            <p className="font-[Jua] text-3xl mt-1 text-white pr-16">한글/영어 선택해줘!</p>
           </div>
           {/* 한글 영어 2 div */}
-          <div>
-            <input
-              type="text"
-              placeholder="이름 쓰기"
-              className="mt-4  px-5 h-[23%] text-3xl font-['Jua'] rounded-full "
-            />
+          <div className="flex gap-5 w-[95%]  ">
+            <button className=" w-1/2 h-10 bg-white text-[#898989] rounded-full text-2xl mt-3 font-jua">
+              한글(Ko)
+            </button>
+            <button className="w-1/2 h-10 bg-white text-[#898989] rounded-full text-2xl mt-3 font-jua">영어(En)</button>
           </div>
         </div>
       </div>
-      {/* 동화책 묻는 칸 + 동화책 선택 리스트 * */}
-      <div>
-        <div className=" w-1/2 h-[23%] bg-[#659AFF] rounded-full">
-          <img src={Pencil} className="absolute w-[80px] h-[100px] top-[-40px] left-[-10px]"></img>
-          <p className=" px-12 py-2 font-jua text-3xl text-white font-jua">무슨 동화책을 고르고싶어?</p>
-          <select className="w-1/2 h-[23%] mt-3 font-jua  p-2 text-3xl absolute">
+      {/* 오른쪽 전체 div  */}
+      <div className="flex flex-col w-[35%] ml-32">
+        {/* 동화책 선택 div 1 */}
+        <div className="w-full h-12 justify-center flex items-center  flex-row bg-[#659AFF] rounded-full">
+          <img src={Pencil} className="w-1/5 mb-10 "></img>
+          <p className="w-full font-jua mt-1 text-3xl text-white">무슨 동화책을 고르고싶어?</p>
+        </div>
+        {/* 동화책 선택 div 2 */}
+        <div className="flex justify-center items-center w-full">
+          <select className="w-2/3 mt-3 p-1 font-jua text-3xl">
             <option value="Snow White">백설 공주</option>
             <option value="Three pig">아기 돼지 삼형제</option>
             <option value="Cinderella">신데렐라</option>
