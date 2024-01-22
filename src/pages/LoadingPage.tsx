9196f865b7d7467a8ded4952d2e067281565ab06
youlimsongs <giuhki@gmail.com>
2024-01-23 01:52:08 +0900

    feat: 로딩 페이지 구현완료

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
 import Lottie from 'lottie-react';
-import bookload from '@/assets/lottie/bookload.json';
+import book from '@/assets/lottie/loading book.json';
+// import bar from '@/assets/lottie/loading bar.json';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const LoadingPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      navigate('/library');
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, [navigate]);
+
   return (
-    <div className="flex justify-center w-screem h-screen">
-      <Lottie animationData={bookload} className="w-[30%]" />
+    <div className="flex flex-col justify-center items-center w-screen h-screen bg-[#bddaff]">
+      <Lottie animationData={book} className="-mt-32" />
+      <p className="font-dongle text-white text-[2.5rem] -mt-32">책을 만들고 있어요!</p>
+      {/* <Lottie animationData={bar} className="w-[20%] h-[30%] -mt-56" /> */}
     </div>
   );
 };
