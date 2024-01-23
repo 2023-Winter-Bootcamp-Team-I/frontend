ce24fa4e63134b7f91dd15bd2d9e52cd44a4ac9b
SeHoon <wer5136@naver.com>
2024-01-23 21:56:13 +0900

    sweetalert

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -3,6 +3,8 @@ import pencilImg from '@/assets/images/pencil.svg';
 import { Link } from 'react-router-dom';
 import { signUpUser } from '@/api/users';
 import { motion } from 'framer-motion';
+import Swal from 'sweetalert2';
+import { useHistory } from 'react-router-dom';
 
 interface SignUpData {
   password: string;
@@ -10,7 +12,8 @@ interface SignUpData {
   name: string;
 }
 
-function SignUp() {
+const SignUp: React.FC = () => {
+  const history = useHistory();
   const [userData, setUserData] = useState<SignUpData>({
     password: '',
     email: '',
@@ -23,7 +26,19 @@ function SignUp() {
   };
 
   const handleSignUp = async () => {
-    await signUpUser(userData);
+    try {
+      await signUpUser(userData);
+      // 만약 signUpUser가 에러 없이 성공하면, '/' 경로로 이동
+      history.push('/');
+    } catch (error) {
+      // signUpUser에서 에러가 발생하면, Swal.fire를 사용하여 에러 메시지 표시
+      Swal.fire({
+        title: '에러!',
+        text: '가입에 실패했습니다. 다시 시도해주세요.',
+        icon: 'error',
+        confirmButtonText: '확인',
+      });
+    }
   };
   console.log(import.meta.env.VITE_BASE_URL_DEV);
   return (
@@ -112,7 +127,7 @@ function SignUp() {
                     가입 취소
                   </motion.button>
                 </Link>
-                <Link to="/" className="flex basis-1/2 justify-center">
+                <div className="flex basis-1/2 justify-center">
                   <motion.button
                     className="flex justify-center font-jua rounded-full bg-signupButtonBlue px-14 py-2.5 text-lg font-bold leading-6 text-white border-b-2 border-r-2 border-shadowGray ml-2"
                     whileHover={{ scale: 1.1 }}
@@ -121,7 +136,7 @@ function SignUp() {
                   >
                     가입 완료
                   </motion.button>
-                </Link>
+                </div>
               </div>
             </div>
           </div>
@@ -129,6 +144,6 @@ function SignUp() {
       </div>
     </>
   );
-}
+};
 
 export default SignUp;
